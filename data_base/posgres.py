c67e70a1d5b57bc1d9a3d0ffb672734aac2dfa9c
Mario <mariochavez1292@gmail.com>
2022-09-19 11:40:20 -0500

    some database functions added

--- data_base/posgres.py
+++ data_base/posgres.py
@@ -1,15 +1,38 @@
 import psycopg2
+import numpy as np
 
-def conection_history():
+def connection_history():
     try:
         connection = psycopg2.connect(
             host = 'localhost',
             user = 'postgres',
             password = 'jumped8285',
-            databaes = 'test'
+            database = 'prueba'
         )
         return connection
     except Exception as ex:
         print(ex)
         return None
 
+def consulta(connection):
+    cursor = connection.cursor()
+    cursor.execute('SELECT id, nombre FROM persona;')
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+def alta(connection, id, nombre):
+    cursor = connection.cursor()
+    cursor.execute(f"INSERT INTO public.persona (id, nombre) VALUES ({id}, '{nombre}');")
+    connection.commit()
+
+def main():
+    connection = connection_history()
+    consulta(connection)
+    for i in range(5):
+        alta(connection, np.random.randint(0, 10000), 'jose')
+    consulta(connection)
+
+
+if __name__ == '__main__':
+    main()
